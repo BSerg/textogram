e97f8e6c92c79e66f3aa50ce255952a504559045
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2016-12-12 15:34:18 +0300

    init

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+#! coding:utf-8
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
