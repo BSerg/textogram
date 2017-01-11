3f0b5fea86420464e7d8b88f330a58b82efe324f
mihailkuznetsov <mihasiksli@yandex.ru>
2017-01-11 17:47:47 +0300

    notification action

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^(articles/[\-\w]+|articles/[\-\w]+/edit|profile/\w+)?/?$', TemplateView.as_view(template_name='index.html'))
+    url(r'^(articles/[\-\w]+|articles/[\-\w]+/edit|profile/\w+|drafts|manage)?/?$', TemplateView.as_view(template_name='index.html'))
 ]
