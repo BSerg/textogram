76ff171308fd91fc4cd291f9183be7464348d9c5
mihailkuznetsov <mihasiksli@yandex.ru>
2017-01-17 17:32:11 +0300

    registration modal

--- api/v1/accounts/views.py
+++ api/v1/accounts/views.py
@@ -10,12 +10,16 @@ from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 from rest_framework import mixins
 
-from accounts.models import User, Subscription, SocialLink
+from accounts.models import User, Subscription, SocialLink, PhoneCode
 from api.v1.accounts.permissions import IsAdminOrOwner
 from api.v1.accounts.serializers import MeUserSerializer, UserSerializer, PublicUserSerializer, MeSocialLinkSerializer, SubscriptionSerializer
 
 from django.core.validators import URLValidator
 from django.forms import ValidationError
+import re
+
+from django.contrib.auth import login
+from rest_framework.authtoken.models import Token
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -104,8 +108,62 @@ class SocialLinksViewSet(viewsets.ModelViewSet):
 
 class Registration(APIView):
 
+    PHONE_PATTERN = '^\d{10}$'
+
+    permission_classes = [permissions.AllowAny]
+
     def post(self, request):
         print request.data.keys()
+        phone = request.data.get('phone')
+        existing_user = User.objects.filter(phone=phone)
+        if existing_user:
+            return Response({'msg': 'error'}, status=HTTP_400_BAD_REQUEST)
+        if 'phone' in request.data.keys() and ('code' not in request.data.keys() and 'hash' not in request.data.keys()):
+            if phone:
+                pattern = re.compile(self.PHONE_PATTERN)
+                if pattern.match(phone):
+                    code = PhoneCode.objects.create(phone=phone)
+                    return Response({'phone': code.phone})
+        elif 'phone' in request.data.key() and 'code' in request.data.keys() and 'hash' not in request.data.keys():
+            code_string = request.data.get('code')
+            if phone and code_string:
+                code = PhoneCode.objects.filter(phone=phone, code=code_string, disabled=False, is_confirmed=False).first()
+                if code and code.is_active():
+                    code.is_confirmed = True
+                    code.save()
+                    return Response({'phone': phone, 'hash': code.hash})
+        elif 'phone' in request.data.key() and 'hash' in request.data.keys() and 'code' not in request.data.keys():
+            hash_string = request.data.get('hash')
+            password = request.data.get('password')
+            password_again = request.data.get('password_again')
+            display_name = request.data.get('username')
+
+            if phone and hash_string and password and password_again and display_name:
+                code = PhoneCode.objects.get(phone=phone, hash=hash_string, disabled=False).first()
+
+                if not code or (password != password_again):
+                    return Response({'msg': 'error'}, status=HTTP_400_BAD_REQUEST)
+                display_name = ' '.join(display_name.split())
+                display_name = display_name.strip()
+                first_name = display_name.split(' ')[0]
+                if not first_name:
+                    return Response({'msg': 'error'}, status=HTTP_400_BAD_REQUEST)
+                last_name = ' '.join(display_name.split(' ')[1:])
+                last_name = last_name.strip()
+                try:
+
+                    user = User.objects.create_user(phone, password=password, phone=phone,
+                                                    first_name=first_name, last_name=last_name)
+
+                    login(request, user)
+                    user_data = MeUserSerializer(user).data
+                    user_data.update(token=Token.objects.get_or_create(user=user)[0].key)
+                    user_data.update(created=True)
+                    return Response({'user': user_data})
+
+                except:
+                    return Response({'msg': 'error'})
+
         return Response({'msg': ''}, status=HTTP_400_BAD_REQUEST)
 
 
