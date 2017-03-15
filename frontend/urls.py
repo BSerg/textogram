20fcfbe9951c46333271085b1d62d7153d8dc339
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-03-15 16:05:29 +0300

    article render meta

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from django.views.generic.base import TemplateView
 
+from frontend.views import ArticleView
+
 urlpatterns = [
-    url(r'^(articles/[\-\w]+|articles/[\-\w]+/edit|profile/\w+|drafts|manage|auth/twitter)?/?$',
-        TemplateView.as_view(template_name='index.html'))
+    url(r'articles/(?P<slug>[\-\w]+)/?$', ArticleView.as_view(), name='article'),
+    url(r'^(articles/[\-\w]+/edit|profile/\w+|drafts|manage|auth/twitter)?/?$',
+        TemplateView.as_view(template_name='index.html')),
+
 ]
---
+++ frontend/views.py
@@ -0,0 +1,26 @@
+from django.urls import reverse
+from django.views.generic import DetailView
+
+from articles import ArticleContentType
+from articles.models import Article
+
+
+class ArticleView(DetailView):
+    queryset = Article.objects.filter(status=Article.PUBLISHED)
+    template_name = 'article.html'
+    context_object_name = 'article'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        article = self.get_object()
+        ctx = super(ArticleView, self).get_context_data(**kwargs)
+
+        description = None
+        if len(article.content.get('blocks')) and article.content['blocks'][0].get('type') == ArticleContentType.LEAD:
+            description = article.content['blocks'][0].get('value')[:200]
+        ctx['description'] = description
+        ctx['image'] = self.request.build_absolute_uri(article.content.get('cover_clipped', {}).get('image') or
+                                                       article.content.get('cover', {}).get('image'))
+        ctx['url'] = self.request.build_absolute_uri(reverse('article', kwargs={'slug': article.slug}))
+        return ctx
