69c0d0ad9c7d8e668cf0e5fcb62bb88fc669df9b
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-03-22 09:19:06 +0300

    fix the right banner + article editor tools

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.base import TemplateView
 from frontend.views import ArticleView
 
 urlpatterns = [
+    url(r'articles/new/?$', TemplateView.as_view(template_name='index.html'), name='article_new'),
     url(r'articles/(?P<slug>[\-\w]+)/?$', ArticleView.as_view(), name='article'),
     url(r'^(articles/[\-\w]+/edit|articles/(?P<id>\d+)/preview|profile/\w+|drafts|manage|auth/twitter)?/?$',
         TemplateView.as_view(template_name='index.html')),
