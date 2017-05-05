04b180f82907d4a5e157b43fad5793ec3c321445
mihailkuznetsov <mihasiksli@yandex.ru>
2017-05-05 15:17:53 +0300

    article preview changes

--- api/v1/articles/views.py
+++ api/v1/articles/views.py
@@ -32,7 +32,7 @@ from textogram.settings import RQ_PORT
 
 
 class ArticleSetPagination(PageNumberPagination):
-    page_size = 50
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
 
