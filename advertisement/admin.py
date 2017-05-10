ffe01358c90d63c49509a03a7633f3b225467063
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-05-10 16:32:44 +0300

    update banners

--- advertisement/admin.py
+++ advertisement/admin.py
@@ -1,3 +1,4 @@
+#! coding: utf-8
 from __future__ import unicode_literals
 from django.contrib import admin
 
@@ -10,8 +11,14 @@ class BannerInline(admin.StackedInline):
 
 
 class BannerGroupAdmin(admin.ModelAdmin):
-    list_display = ['identifier', 'is_mobile', 'is_active']
+    list_display = ['identifier', 'get_banner_count', 'is_mobile', 'is_active']
     inlines = [BannerInline]
+    list_filter = ['identifier', 'is_mobile', 'is_active']
+
+    def get_banner_count(self, obj):
+        return obj.banners.count()
+
+    get_banner_count.short_description = 'Кол-во баннеров'
 
 
 admin.site.register(BannerGroup, BannerGroupAdmin)
