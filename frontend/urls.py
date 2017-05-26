cbd6d345b11ff3bc54c9fc457e6fa62c0c36d0a5
Sergei Bychkov <bychkov.sergei.vladimirovich@gmail.com>
2017-05-26 06:36:55 +0300

    new gallery activating

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,21 +1,14 @@
-from django.conf.urls import url, include
-from django.views.generic.base import TemplateView
-from .views import BaseTemplateView
-from textogram.settings import IS_LENTACH
+from django.conf.urls import url
 
 from frontend.views import ArticleView, EditorView
+from textogram.settings import IS_LENTACH
+from .views import BaseTemplateView
 
 urlpatterns = [
     url(r'articles/new/?$', BaseTemplateView.as_view(template_name='index.html'), name='article_new'),
-    url(r'articles/(?P<slug>[\-\w]+)(/gallery/[\w\-]+)?(/new)?/?$', ArticleView.as_view(), name='article'),
+    url(r'articles/(?P<slug>[\-\w]+)(/gallery/[\w\-]+)?/?$', ArticleView.as_view(), name='article'),
     url(r'articles/(?P<pk>[\-\w]+)/edit?$', EditorView.as_view(), name='editor'),
-
-    #url(r'^(login|articles/[\-\w]+/edit|articles/(?P<id>\d+)/preview|id\d+|id\d+/\w+|drafts|manage|manage/\w+|feed|auth/twitter|url_shorten)?/?$',
-    #    BaseTemplateView.as_view(template_name='index.html')),
 ]
-
-# urlpatterns.append(url(r'^(login|articles/[\-\w]+/edit|articles/(?P<id>\d+)/preview|id\d+|id\d+/\w+|drafts|manage|manage/\w+|feed|auth/twitter|url_shorten)?/?$',
-#         BaseTemplateView.as_view(template_name='index.html')) )
 
 if IS_LENTACH:
     urlpatterns += [url(r'^(login|articles/[\-\w]+/edit|articles/(?P<id>\d+)/preview|id\d+|id\d+/\w+|drafts|manage|manage/\w+|feed|auth/twitter|url_shorten)?/?$',
