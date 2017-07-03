e54a8700718f2141d38e1e53da548d414fde39c0
mihailkuznetsov <mihasiksli@yandex.ru>
2017-07-03 21:23:43 +0300

    user subscribe feed cache

--- articles/management/commands/save_all_views_to_db.py
+++ articles/management/commands/save_all_views_to_db.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 
 from django.core.management.base import BaseCommand
-from articles.cache_utils import save_all_cached_views_to_db
+from articles.cache_utils import save_cached_views_to_db
 
 
 class Command(BaseCommand):
     help = 'Save all views to db'
 
     def handle(self, *args, **options):
-        save_all_cached_views_to_db()
+        save_cached_views_to_db()
