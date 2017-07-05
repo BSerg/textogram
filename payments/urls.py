55354ded1c0da5bc3c8d0438f99fdb330d2f32d1
Сергей Бычков <bychkov.sergei.vladimirovich@gmail.com>
2017-07-05 15:10:31 +0300

    csrf fix

--- payments/urls.py
+++ payments/urls.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
-
 
 urlpatterns = [
     url(r'yandex/check-order/', views.YandexCheckoutCheckView.as_view(), name='yandex_check_order'),
     url(r'yandex/payment-aviso/', views.YandexCheckoutPaymentAviso.as_view(), name='yandex_payment_aviso'),
-    url(r'walletone/check-order/', ensure_csrf_cookie(views.WalletoneOrderCheck.as_view()), name='walletone_check_order'),
+    url(r'walletone/check-order/', csrf_exempt(views.WalletoneOrderCheck.as_view()), name='walletone_check_order'),
 ]
+
