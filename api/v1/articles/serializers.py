e0bc67822cd3bd0432062eec59f1823473f36186
Сергей Бычков <bychkov.sergei.vladimirovich@gmail.com>
2017-07-18 16:47:40 +0300

    article image serializer extended

--- api/v1/articles/serializers.py
+++ api/v1/articles/serializers.py
@@ -26,6 +26,8 @@ class ArticleImageSerializer(serializers.ModelSerializer):
     medium = serializers.SerializerMethodField()
     preview = serializers.SerializerMethodField()
     original = serializers.SerializerMethodField()
+    original_width = serializers.IntegerField(source='image.width', read_only=True)
+    original_height = serializers.IntegerField(source='image.height', read_only=True)
 
     def get_small(self, obj):
         return obj.get_image_url(THUMBNAIL_SMALL_SIZE)
