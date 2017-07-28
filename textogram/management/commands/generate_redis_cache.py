36eee5e0f84515cdd4cb976589ff29bf052c16b3
Сергей Бычков <bychkov.sergei.vladimirovich@gmail.com>
2017-07-28 12:13:13 +0300

    update

--- textogram/management/commands/generate_redis_cache.py
+++ textogram/management/commands/generate_redis_cache.py
@@ -1,10 +1,14 @@
 from __future__ import unicode_literals
 
+from django.core.management import call_command
+
 from advertisement.cache_utils import update_advertisements_cache
 from articles.cache_utils import update_article_cache, update_feed_cache, generate_search_index, \
     cache_articles_views_count, update_short_url_cache
 from accounts.cache_utils import update_user_cache
 from redis import StrictRedis
+
+from articles.management.commands import update_article_recommendations
 from textogram.settings import REDIS_CACHE_DB, REDIS_CACHE_HOST, REDIS_CACHE_PORT
 
 from django.core.management.base import BaseCommand
@@ -23,3 +27,4 @@ class Command(BaseCommand):
         update_user_cache()
         update_short_url_cache()
         update_advertisements_cache()
+        call_command('update_article_recommendations')
