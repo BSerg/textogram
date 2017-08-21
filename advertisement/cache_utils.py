8079ac87b26d2514f34974f0bae521cf4313ac6b
Сергей Бычков <bychkov.sergei.vladimirovich@gmail.com>
2017-08-21 16:48:08 +0300

    add refactored banners to cache

--- advertisement/cache_utils.py
+++ advertisement/cache_utils.py
@@ -5,12 +5,17 @@ import json
 
 from redis import StrictRedis
 
-from advertisement.utils import serialize_banners
+from advertisement.utils import serialize_banners, serialize_banners2
 from textogram.settings import REDIS_CACHE_DB, REDIS_CACHE_HOST, REDIS_CACHE_PORT, REDIS_CACHE_KEY_PREFIX
 
 
 def update_advertisements_cache():
     r = StrictRedis(host=REDIS_CACHE_HOST, port=REDIS_CACHE_PORT, db=REDIS_CACHE_DB)
+
     banners = serialize_banners()
     if banners:
         r.set('%s:advertisements:banners' % REDIS_CACHE_KEY_PREFIX, json.dumps(banners))
+
+    banners2 = serialize_banners2()
+    if banners2:
+        r.set('%s:advertisements:banners2' % REDIS_CACHE_KEY_PREFIX, json.dumps(banners2))
